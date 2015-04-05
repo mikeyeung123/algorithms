cfbdfc5a5ea8fb22b7ccd974188541752beefbc5
Mike Yeung <mikeyeung123@gmail.com>
2015-04-04 21:14:28 -0400

    Implemented delete in RedBlackTree.swift. Not tested. Most definitely doesn't work yet.

--- RedBlackTree.swift
+++ RedBlackTree.swift
@@ -13,7 +13,7 @@ private func oppositeDirection(direction: Direction) -> Direction {
 }
 
 private enum Color: Int {
-    case Red, Black
+    case Red, Black, DoubleBlack
 }
 
 // Initializer handles adding dummy nodes
@@ -40,7 +40,12 @@ private class Node<T> {
         }
     }
     
+    func sibling() -> Node? {
+        return parent?.children![oppositeDirection(childType!).rawValue]
+    }
+    
     func switchColor() {
+        assert(color != .DoubleBlack, "Cannot switch DoubleBlack")
         color = Color(rawValue: (color.rawValue + 1) % 2)!
     }
 }
@@ -91,24 +96,24 @@ class RedBlackTree<T: Comparable> {
     private func recolorUpwards(var node: Node<T>) {
         if node.parent == nil || node.parent!.color == .Black {
             root!.color == .Black
-            return
-        }
-        var parent = node.parent!
-        let grandparent = parent.parent!
-        let uncle = grandparent.children![oppositeDirection(parent.childType!).rawValue]
-        if uncle.color == .Red {
-            parent.switchColor()
-            grandparent.switchColor()
-            uncle.switchColor()
-            recolorUpwards(grandparent)
         } else {
-            if node.childType == oppositeDirection(parent.childType!) {
-                rotate(parent, direction: parent.childType!)
-                swap(&node, &parent)
+            var parent = node.parent!
+            let grandparent = parent.parent!
+            let uncle = grandparent.children![oppositeDirection(parent.childType!).rawValue]
+            if uncle.color == .Red {
+                parent.switchColor()
+                grandparent.switchColor()
+                uncle.switchColor()
+                recolorUpwards(grandparent)
+            } else {
+                if node.childType == oppositeDirection(parent.childType!) {
+                    rotate(parent, direction: parent.childType!)
+                    swap(&node, &parent)
+                }
+                rotate(grandparent, direction: oppositeDirection(parent.childType!))
+                grandparent.switchColor()
+                parent.switchColor()
             }
-            rotate(grandparent, direction: oppositeDirection(parent.childType!))
-            grandparent.switchColor()
-            parent.switchColor()
         }
     }
     
@@ -123,6 +128,95 @@ class RedBlackTree<T: Comparable> {
                 result.node!.parent!.children![result.node!.childType!.rawValue] = toInsert
                 recolorUpwards(toInsert)
             }
+        }
+    }
+    
+    private func successor(node: Node<T>) -> Node<T> {
+        assert(node.children != nil && node.children![1].value != nil, "successor() must be called on node with right non-leaf child")
+        var iter = node.children![1]
+        while iter.children![0].value != nil {
+            iter = iter.children![0]
+        }
+        return iter
+    }
+    
+    private func resolveDoubleBlack(node: Node<T>) {
+        if node.parent == nil {
+            node.color = .Black
+        } else if node.color == .DoubleBlack {
+            let parent = node.parent!
+            let sibling = node.sibling()!
+            if sibling.color == .Black {
+                let redChildren = sibling.children!.filter{$0.color == .Red}
+                if redChildren.count > 0 {
+                    let sameDirectionChildren = redChildren.filter{$0.childType == sibling.childType}
+                    let differentDirectionChildren = redChildren.filter{$0.childType != sibling.childType}
+                    if sameDirectionChildren.count == 1 {
+                        rotate(parent, direction: node.childType!)
+                        sameDirectionChildren[0].color = .Black
+                        node.color = .Black
+                    } else {
+                        rotate(sibling, direction: sibling.childType!)
+                        rotate(parent, direction: node.childType!)
+                        differentDirectionChildren[0].color = .Black
+                        node.color = .Black
+                    }
+                } else {
+                    sibling.color = .Red
+                    node.color = .Black
+                    if parent.color == .Red {
+                        parent.color = .Black
+                    } else {
+                        parent.color = .DoubleBlack
+                        resolveDoubleBlack(parent)
+                    }
+                }
+            } else {
+                rotate(parent, direction: node.childType!)
+                sibling.color = .Black
+                node.color = .Black
+                node.sibling()!.color = .Red
+            }
+        }
+    }
+    
+    private func delete(node: Node<T>) {
+        assert(node.value != nil, "Cannot delete dummy node")
+        let children = node.children!.filter{$0.value != nil}
+        if children.count == 0 {
+            if node.parent == nil {
+                root = nil
+            } else if node.color == .Red {
+                node.value = nil
+                node.color = .Black
+                node.children = nil
+            } else {
+                node.value = nil
+                node.color = .DoubleBlack
+                node.children = nil
+                resolveDoubleBlack(node)
+            }
+        } else if children.count == 1 {
+            assert(children[0].color == .Red, "If only child isn't red, violates red-black tree properties, probably from a bug in insert()")
+            children[0].color == .Black
+            children[0].parent = node.parent
+            if node.parent == nil {
+                root = children[0]
+            } else {
+                node.parent!.children![node.childType!.rawValue] = children[0]
+            }
+        } else {
+            let s = successor(node)
+            node.value = s.value
+            delete(s)
+        }
+    }
+    
+    // If value exists, delete it
+    func delete(value: T) {
+        let result = search(value: value, node: root)
+        if result.found {
+            delete(result.node!)
         }
     }
 }
